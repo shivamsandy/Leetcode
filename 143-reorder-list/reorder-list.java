f6a7553bb20f23f19e76e2b10ac9746799f604e9
shivamsandy <shivamsandy3183@gmail.com>
2026-04-27 04:12:50 +0530

    Time: 4 ms (10.52%) | Memory: 50.9 MB (6.88%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -10,26 +10,30 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-       if (head == null || head.next == null) return;
+        if (head == null || head.next == null)
+            return;
 
-    Stack<ListNode> st = new Stack<>();
-    ListNode temp = head;
-    while (temp != null) {
-        st.push(temp);
-        temp = temp.next;
-    }
+        Stack<ListNode> st = new Stack<>();
+        ListNode temp = head;
+        while (temp != null) {
+            st.push(temp);
+            temp = temp.next;
+        }
 
-    int size = st.size();
-    temp = head;
-    for (int i = 0; i < size / 2; i++) {
-        ListNode last = st.pop();
+        int size = st.size();
+        temp = head;
+        for (int i = 0; i < size / 2; i++) {
+            ListNode last = st.pop();
 
-        ListNode nextNode = temp.next;
-        temp.next = last;
-        last.next = nextNode;
+            ListNode nextNode = temp.next;
+            temp.next = last;
+            last.next = nextNode;
 
-        temp = nextNode;
-    }
-    temp.next = null;
+            temp = nextNode;
+        }
+        temp.next = null;
+
+        //  DO IT USING BEST TIME COMPLEXCITY 
+        
     }
 }
